c0604101f66cc639f3bb4c5ff399899dc46553d9
Xwilarg <xwilarg@protonmail.com>
2023-12-24 13:22:38 +0100

    Add WebGL conditions

---
+++ Game/Assets/Scripts/WebGL/EnableOnlyIfNotWebGL.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace JameGam.WebGL
+{
+    public class EnableOnlyIfNotWebGL : MonoBehaviour
+    {
+        private void Awake()
+        {
+            if (Application.platform == RuntimePlatform.WebGLPlayer)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Game/Assets/Scripts/WebGL/EnableOnlyIfWebGL.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace JameGam.WebGL
+{
+    public class EnableOnlyIfWebGL : MonoBehaviour
+    {
+        private void Awake()
+        {
+            if (Application.platform != RuntimePlatform.WebGLPlayer)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
